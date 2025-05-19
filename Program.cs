a2fdfb28a9a90e71e7b3c66728a86952676df0ca
Beatriz Batista <byah24@outlook.com>
2025-05-19 08:27:40 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿string resposta;
+
+Console.WriteLine("Conheça o Meme Fluxo da Engenharia!");
+Console.WriteLine("Está se movendo? (Responda com Sim ou Não): ");
+
+resposta = Console.ReadLine()!.ToUpper();
+
+if (resposta == "SIM")
+{
+    Console.WriteLine("Deveria? (Responda com Sim ou Não): ");
+    resposta = Console.ReadLine()!.ToUpper();
+
+    if (resposta == "SIM")
+    {
+        Console.WriteLine("Ótimo");
+    }
+    else
+    {
+        Console.WriteLine("Use Silver Tape");
+    }
+
+}
+else
+{
+    Console.WriteLine("Deveria? (Responda com Sim ou Não): ");
+    resposta = Console.ReadLine()!.ToUpper();
+
+    if (resposta == "NAO")
+    {
+        Console.WriteLine("Use WD-40");
+    }
+    else
+    {
+        Console.WriteLine("Ótimo!");
+    }
+
+}
